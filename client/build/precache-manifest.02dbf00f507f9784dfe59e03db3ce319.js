68538c8d32a546c2bfaa57dbebc83db73c742ed2
gabbayron <gabbayron2307@gmail.com>
2020-10-08 18:08:46 +0300

    build

---
+++ client/build/precache-manifest.02dbf00f507f9784dfe59e03db3ce319.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8956af7c3f5a88c9ee450f14488023b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ef127d9821a1f2d9d69",
+    "url": "/static/css/main.ad0698d3.chunk.css"
+  },
+  {
+    "revision": "6ba9c21cca40f8a56f84",
+    "url": "/static/js/2.9c84fa14.chunk.js"
+  },
+  {
+    "revision": "d25ce25f9b750c9a0e0f3c8eba8f1ee1",
+    "url": "/static/js/2.9c84fa14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ef127d9821a1f2d9d69",
+    "url": "/static/js/main.3d7d7ce0.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
